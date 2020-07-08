01c6b2d52cb0c004ad25cb5c03e4affcf85cca0a
jrladeras <51767097+jrladeras@users.noreply.github.com>
2020-07-09 01:55:16 +0800

    count overall changes

--- backend/classess/CounterClass.js
+++ backend/classess/CounterClass.js
@@ -1,4 +1,8 @@
-const MDB_COMPANIES = require('../models/MDB_COMPANIES');
+const MDB_COMPANIES     = require('../models/MDB_COMPANIES');
+const MDB_COUNT_OVERALL = require('../models/MDB_COUNT_OVERALL');
+const MDB_COUNT_DAILY   = require('../models/MDB_COUNT_DAILY')
+const MDB_COUNT_MONTHLY = require('../models/MDB_COUNT_MONTHLY');
+
 const FormData = require('form-data');
 const axios = require('axios');
 const fs = require('fs');
@@ -7,27 +11,30 @@ module.exports = class CounterClass
 {
     constructor()
     {
-        this.company_to_update
-        this.company_id = '5f05db06e58ab828e0450022'
+        this.company_to_update = []
+        this.company_id = '5f05fe99a3ed7128705f680d'
     }
     async getCompany(company_id)
     {
-        console.log(company_id);
         let company_details = await new MDB_COMPANIES().doc(company_id)
-        console.log(company_details);
-        
-        // if (company_details.parent_id == 'No Parent')
-        // {
-        //     console.log('Done');
-        // }
-        // else
-        // {
-        //     this.getCompany(company_details.parent_id) 
-        // }
+        if (company_details.parent_id != 'No Parent')
+        {
+            this.company_to_update.push(company_details._id)
+            this.getCompany(company_details.parent_id) 
+        }
     }
 
     async counterActivities(key, date_string)
     {
+        this.company_to_update.push(this.company_id)
         this.getCompany(this.company_id)
+
+        for ( let company_id of this.company_to_update )
+        {
+            let company = await new MDB_COUNT_OVERALL().docs({company_id: company_id})
+            if (company.length)await new MDB_COUNT_OVERALL().collection.where({company_id: company_id}).updateMany({$inc: {count: 1}, key: 'Traffic'})
+            else await new MDB_COUNT_OVERALL().add({count: 1, key: 'Traffic', company_id: company_id})
+             
+        }
     }
 }
--- backend/models/MDB_COUNT_OVERALL.js
+++ backend/models/MDB_COUNT_OVERALL.js
@@ -6,7 +6,7 @@ const schema    = new Schema(
 {
     company_id: { type: String, required: true },
     key: { type: String, required: true },
-    count: { type: String, required: true },
+    count: { type: Number, required: true , default: 0},
 });
 
 class MDB_COUNT_OVERALL extends MODEL
