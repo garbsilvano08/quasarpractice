0bda769302e86e351a90fd48a56c1f3d4be058cd
jrladeras <51767097+jrladeras@users.noreply.github.com>
2020-08-24 17:03:26 +0800

    saved

--- frontend/src/pages/Member/DeviceManagement/AddDevice.vue
+++ frontend/src/pages/Member/DeviceManagement/AddDevice.vue
@@ -130,7 +130,6 @@ export default {
                     let req = await this.$_post(postAddDevice, {device_info: device_info});
                     // let logs = await this.$_post('member/visionsky/logs');
                     // console.log(logs);
-                    console.log(req);
                     // console.log(this.log_type);
                     if (this.device_type == 'vision_sky')
                     {
@@ -139,7 +138,6 @@ export default {
                         data.append('pass', 'abc123');
                         data.append('callbackUrl', 'http://192.168.254.120:4001/api/member/visionsky/logs');
                         let logs = await this.$axios.post("http://" + this.input_device_ip + ":8090/setIdentifyCallBack", data).then(res => res.data);
-                        console.log(logs);
                     }
 
                     this.$q.loading.hide();
