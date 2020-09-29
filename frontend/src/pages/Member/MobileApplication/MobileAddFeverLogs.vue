ebb2b3de02e7c990c34decc5fb37f566f2c76885
raelyo <raeleusebioo@gmail.com>
2020-09-29 13:48:26 +0800

    fixed #68

--- frontend/src/pages/Member/MobileApplication/MobileAddFeverLogs.vue
+++ frontend/src/pages/Member/MobileApplication/MobileAddFeverLogs.vue
@@ -6,7 +6,16 @@
                     <!-- PERSONAL INFORMATION -->
                     
                     <div class="frontdesk__content-info">
-                        <div class="content__select">
+                        <div class="content__select-img">
+                            <div class="content__img-border">
+                                <canvas class="content__img"  border icon="mdi-camera" id="canvas" width="340" height="280"></canvas>
+                                <!-- <video v-show="is_carturing" class="content__img" id="video" width="500" height="500" autoplay></video> -->
+                                <!-- <q-img class="content__img" :src="personal_information.account_img ? personal_information.account_img : '/img/placeholder-img.jpg'"></q-img> -->
+                                <!-- <input style="display:none" capture="camera" id="uploadImage" accept="image/*" @change="uploadImage()" ref="uploader" type="file"> -->
+                                <q-btn class="btn-upload btn-primary" flat dense no-caps>
+                                    <q-icon style="font-size: 13px; margin-right: 3px" name="fas fa-camera"></q-icon> Capture
+                                </q-btn>
+                            </div>
                             <div class="content__img-border">
                                 <canvas class="content__img"  border icon="mdi-camera" id="canvas" width="340" height="280"></canvas>
                                 <!-- <video v-show="is_carturing" class="content__img" id="video" width="500" height="500" autoplay></video> -->
@@ -20,39 +29,39 @@
                         <input style="display:none" id="uploadImage" accept="image/*" @change="uploadImage()" ref="uploader" type="file">
                         <q-btn class="btn-upload btn-primary" flat dense no-caps label="Take a Photo" @click="openFilemanager()"></q-btn> -->
                         </div>
-                    <div class="frontdesk__content">
-                        <div class="content__input">
-                            <div class="content__input-label">Temperature Check</div>
-                            <q-input outlined dense></q-input>
-                        </div>
-                        <div class="header__title">Personal Information</div>
-                        <div class="content__input">
-                            <div class="content__input-label">First name*</div>
-                            <q-input outlined dense></q-input>
-                        </div>
-                        <div class="content__input">
-                            <div class="content__input-label">Last name*</div>
-                            <q-input outlined dense></q-input>
-                        </div>
-                        <div class="content__input">
-                            <div class="content__input-label">Middle name*</div>
-                            <q-input outlined dense></q-input>
-                        </div>
-                        <div class="frontdesk__content-grid">
-                            <div class="content__select">
-                                <div class="content__select-label">Gender*</div>
-                                <q-select outlined dense></q-select>
+                        <div class="frontdesk__content">
+                            <div class="content__input">
+                                <div class="content__input-label">Temperature Check</div>
+                                <q-input outlined dense></q-input>
+                            </div>
+                            <div class="header__title">Personal Information</div>
+                            <div class="content__input">
+                                <div class="content__input-label">First name*</div>
+                                <q-input outlined dense></q-input>
                             </div>
                             <div class="content__input">
-                                <div class="content__input-label">Birthdate*</div>
+                                <div class="content__input-label">Last name*</div>
+                                <q-input outlined dense></q-input>
+                            </div>
+                            <div class="content__input">
+                                <div class="content__input-label">Middle name*</div>
+                                <q-input outlined dense></q-input>
+                            </div>
+                            <div class="frontdesk__content-grid">
+                                <div class="content__select">
+                                    <div class="content__select-label">Gender*</div>
+                                    <q-select outlined dense></q-select>
+                                </div>
+                                <div class="content__input">
+                                    <div class="content__input-label">Birthdate*</div>
+                                    <q-input outlined dense></q-input>
+                                </div>
+                            </div>
+                            <div class="content__input">
+                                <div class="content__input-label">Nationality</div>
                                 <q-input outlined dense></q-input>
                             </div>
                         </div>
-                        <div class="content__input">
-                            <div class="content__input-label">Nationality</div>
-                            <q-input outlined dense></q-input>
-                        </div>
-                    </div>
                     
                     </div>
                 </div>
